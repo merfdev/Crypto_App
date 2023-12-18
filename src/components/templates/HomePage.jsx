fb35f1307ab816d02ae97cb47e6628b6757f806f
mohammad erfany <98615272+merfdev@users.noreply.github.com>
2023-12-18 14:33:42 +0330

    add api creator

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import TableCoins from "../modules/TableCoins";
+import { getCoinList } from "../../services/cryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-FKCtssNj4VA4Vj197wM1smE2"
-    ).then((res) => res.json().then((json) => setCoins(json)));
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+    getData();
   }, []);
   return (
     <div>
