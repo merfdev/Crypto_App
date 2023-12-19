9ff2ba0acfd119da5785135141d32a3e7afcdb81
mohammad erfany <98615272+merfdev@users.noreply.github.com>
2023-12-19 12:28:45 +0330

    create table

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,6 +1,51 @@
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
 function TableCoins({ coins }) {
   console.log(coins);
-  return <>aa</>;
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Valume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <TableRow coin={coin} key={coin.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default TableCoins;
+
+const TableRow = ({ coin }) => {
+  return (
+    <tr>
+      <td>
+        <div>
+          <img src={coin.image} />
+          <span>{coin.symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{coin.name}</td>
+      <td>{coin.current_price.toLocaleString()}</td>
+      <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+      <td>{coin.total_volume.toLocaleString()}</td>
+      <td>
+        <img
+          src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown}
+          alt={coin.name}
+        />
+      </td>
+    </tr>
+  );
+};
